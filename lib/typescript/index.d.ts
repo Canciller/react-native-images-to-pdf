a98f3e40a9826cb52fac59c5036df53e1afa2e35
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2023-06-15 19:02:46 -0600

    chore: update lib

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,8 +1,24 @@
-export interface CreatePdfOptions {
-    imagePaths: string[];
+export type ImageFit = 'none' | 'fill' | 'contain' | 'cover';
+export type Page = {
+    imagePath: string;
+    imageFit?: ImageFit;
+    width?: number;
+    height?: number;
+};
+export type CreatePdfOptions = {
     outputDirectory: string;
     outputFilename: string;
-}
+    pages: Array<Page | string>;
+    imagePaths?: undefined;
+} | {
+    outputDirectory: string;
+    outputFilename: string;
+    pages?: undefined;
+    /**
+     * @deprecated Use the `pages` property instead.
+     */
+    imagePaths: string[];
+};
 export declare function createPdf(options: CreatePdfOptions): Promise<string>;
 export declare function getDocumentsDirectory(): Promise<string>;
 //# sourceMappingURL=index.d.ts.map
